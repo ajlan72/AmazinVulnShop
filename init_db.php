7fe0fbc9bc2916e6c97c11abdeeb2ec14fc47f5a
ajlan72 <ajlan072@gmail.com>
2025-11-11 13:43:08 +0530

    Updated

--- init_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = new PDO('sqlite:db.sqlite3');
-$schema = file_get_contents('db.sql');
-$db->exec($schema);
-$db->exec("INSERT INTO users (username, password) VALUES ('admin', 'adminpass');");
-echo "DB initialized.";
-?>
